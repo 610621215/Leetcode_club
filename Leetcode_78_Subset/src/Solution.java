158bfd7bcb444a81150ae7908196e862f6e430be
610621215 <34718352+610621215@users.noreply.github.com>
2021-04-23 13:00:22 +0800

    通過
    新舊版寫錯的地方已經標註於Subset_v2.pdf中

--- Leetcode_78_Subset/src/Solution.java
+++ Leetcode_78_Subset/src/Solution.java
@@ -1,41 +1,37 @@
 import java.util.*;
-
-public class Solution {
+class Solution {
     int[] table=null;
     public List<List<Integer>> subsets(int[] nums) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         table=new int[nums.length];
         List<Integer> list=new ArrayList<>();
         List<List<Integer>> result=new ArrayList<>();
-        result.add(new ArrayList<>());
-        doWork(nums,list,result);
-        for(int i=0;i<nums.length;i++){
-            list=new ArrayList<>();
-            list.add(nums[i]);
-            if (!result.contains(list)){
-                result.add(list);
-            }
-        }
+        // result.add(new ArrayList<>());
+        doWork(nums,0,list,result);
+//        for(int i=0;i<nums.length;i++){
+//            list=new ArrayList<>();
+//            list.add(nums[i]);
+//            if (!result.contains(list)){
+//                result.add(list);
+//            }
+//        }
 
         return result;
     }
+    public void doWork(int[] nums,int step,List<Integer> list,List<List<Integer>>result){
+        ArrayList<Integer> cpy_list=new ArrayList<Integer>();
+        cpy_list=(ArrayList<Integer>)((ArrayList<Integer>)list).clone();
+        // cpy_list.sort((Integer o1 ,Integer o2)->o1-o2);
+        // if (!result.contains(cpy_list)){
+        result.add(cpy_list);
+        // }
 
-    public void doWork(int[] nums,List<Integer> list,List<List<Integer>>result){
-        for(int i=0;i<nums.length;i++){
-            if(table[i]==0) {
-                table[i] = 1;
-                list.add(nums[i]);
-                doWork(nums, list,result);
-                table[i] = 0;
+        for(int i=step;i<nums.length;i++){
+            list.add(nums[i]);
+            // System.out.println(list);
+            doWork(nums, i+1,list,result);
+            list.remove(list.size()-1);
 
-                list.sort((Integer o1, Integer o2)-> o1-o2);
-                if (!result.contains(list)){
-                    result.add(list);
-                }
-                list=new ArrayList<>();
-            }
         }
     }
-
-
 }
